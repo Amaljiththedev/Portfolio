05965d474ceedd25ad68f45e9ecda982be89bd2f
Amaljiththedev <amaljith6430@gmail.com>
2024-06-21 22:17:34 +0530

    Bento grid partially completed2

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FloatingNav } from "@/ui/floating-navbar";
 import { FaHome } from "react-icons/fa";
 import { Imprima } from "next/font/google";
 
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
